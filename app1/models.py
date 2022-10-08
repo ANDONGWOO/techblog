31a75dd161296b54dba2b0cc60394b34596f32da
an dong woo <akskwha@naver.com>
2022-10-08 18:44:07 +0900

    클래스 정의/네비바 구현/README오늘내용 추가

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,11 @@
+from ssl import create_default_context
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class App1(models.Model):
+    title=models.CharField(max_length=20)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
