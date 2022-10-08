557ac68a9addf5786fa6ed3e7643a65b53a0b104
an dong woo <akskwha@naver.com>
2022-10-08 17:46:07 +0900

    base.html 구현

--- techblog/settings.py
+++ techblog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app1',
     #앱추가
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'techblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'techblog' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
