efb2e499fd42b33ca4e9c16b1712847faebc1f35
an dong woo <akskwha@naver.com>
2022-10-10 20:18:40 +0900

    index.html(제목/내용출력)구현/index페이징버튼 배치

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from.models import App1
 # Create your views here.
 
 def index(request):
-    
-    return render(request, 'app1/index.html')
+    app1 = App1.objects.order_by('-pk')
+    context={
+        'app':app1,
+    }
+    return render(request, 'app1/index.html', context)
